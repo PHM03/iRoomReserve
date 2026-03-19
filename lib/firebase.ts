2c3537ced68db2e7c2df2eaae4a2c5e3aaa803cd
Adrian Villanueva Echepari <florie.echepari@gmail.com>
2026-03-19 22:13:00 +0800

    Moved Firebase configuration to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,13 +7,14 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAloRS-cQZ0jJvN3JrA3m_5Wm4xXrMwYFE",
-  authDomain: "iroomreserve.firebaseapp.com",
-  projectId: "iroomreserve",
-  storageBucket: "iroomreserve.firebasestorage.app",
-  messagingSenderId: "735804203844",
-  appId: "1:735804203844:web:b166e2f182e29d367b6d11",
-  measurementId: "G-6PQBSMRE8L",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase (prevent duplicate initialization on hot reload)
